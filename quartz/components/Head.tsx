a4cdda3395beebab1ea325575fb900febb5963f4
themejin <themejin@gmail.com>
2024-06-17 21:33:44 -0400

    Quartz sync: Jun 17, 2024, 9:33 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -15,7 +15,7 @@ export default (() => {
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
 
-    const iconPath = 'https://${cfg.baseUrl}/icon.png'
+    const iconPath = 'https://www.mejinsopentable.com/static/icon.png'
     const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
 
     return (
@@ -30,8 +30,7 @@ export default (() => {
           </>
         )}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:
-        " content={title} />
+        <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
         <meta property="og:width" content="1200" />
